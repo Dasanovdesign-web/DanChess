9c1f1669526010caf24ae56bf60d75b7a9682ab9
Daniar <d.asanovdesign@gmail.com>
2026-02-05 03:35:03 +0600

    Добавил ценность фигурам В шахматах принято что пешка равно 1 единице, конь и слон по 3, ладьи по 5, ферзь на 9 Для чего? В дальнейшем, при каждом ходе будет высчитываться сила хода и преимущество у кого
    Добавил логику хождения Ферзя.Он ходит как Слон и Ладья.

--- pieces.py
+++ pieces.py
@@ -2,6 +2,7 @@ class Knight:
     def __init__(self, color):
         self.color = color
         self.is_alive = True
+        self.value = 3 #Ценность фигуры
         #Беый конь ♘, черный ♞
         self.symbol = "♘" if color == "White" else "♞" 
         self.material = "porcelain" # По умолчаю сдесь будет стандартный скин клетки, в будущем тутможно менять скины доски 
@@ -11,16 +12,15 @@ class Knight:
             row_diff = abs(start_pos[0] - end_pos[0])
             col_diff = abs(start_pos[1] - end_pos[1])
 
-            #Правило буквы Г: (2 и 1) bkb (1 и 2)
+            #Правило буквы Г: (2 и 1) или (1 и 2)
             return (row_diff == 2 and col_diff == 1) or (row_diff == 1 and col_diff == 2)
-
-            
-      
+    
 
 class Pawn:
     def __init__(self, color):
         self.color = color
         self.is_alive = True
+        self.value = 1 #Ценность фигуры
         #Белая пешка ♙, черный ♟
         self.symbol = "♙" if color == "White" else "♟"
 
@@ -38,6 +38,7 @@ class Bishop:
     def __init__(self, color):
         self.color = color
         self.is_alive = True
+        self.value = 3 #Ценность фигуры
         #Белый слон на ♗, черный ♝
         self.symbol = "♗" if color == "White" else "♝"
         pass
@@ -58,6 +59,7 @@ class Rook:
     def __init__(self, color):
         self.color = color
         self.is_alive = True 
+        self.value = 5 #Ценность фигуры
         #Белая ладья ♖, черный ♜
         self.symbol = "♖" if color == "White" else "♜"
      
@@ -78,8 +80,18 @@ class Queen:
     def __init__(self, color):
         self.color = color
         self.is_alive = True
+        self.value = 9 #Ценность фигуры
         #Белый ферзь  ♕, черный ♛
         self.symbol = "♕" if color == "White" else "♛"
+
+    def is_valid_move(self, start_pos, end_pos):
+         row_diff = abs(start_pos[0] - end_pos[0])
+         col_diff = abs(start_pos[1] - end_pos[1])
+         #Логика ладьи (прямо) и логика слона (диагональ)
+         is_rook = (row_diff > 0 and col_diff == 0) or (row_diff == 0 and col_diff > 0)
+         is_bishop = (row_diff == col_diff and row_diff > 0)
+         return is_rook or is_bishop
+
 
 
 
